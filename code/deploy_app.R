2669f9092631afdeb3d4fda72f9772ef0d36a950
ggionet1 <gagi0247@colorado.edu>
2024-06-15 18:51:07 +0000

    attempting app deployment

--- code/deploy_app.R
+++ code/deploy_app.R
@@ -2,8 +2,11 @@ library(rsconnect)
 library(shinylive)
 library(httpuv)
 
+<<<<<<< Updated upstream
 getwd()
 
+=======
+>>>>>>> Stashed changes
 # Connect the r script below to an R shiny app
 rsconnect::setAccountInfo(name='cutrifinio', token='C0BFCA28C1FE0F83CB5E67B30FA01E0E', secret='b6lWE/Jk2CFw+J5Vu6F3zXxgyk/AsEu9iuVcBGxG')
 
